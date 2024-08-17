bce4d0428159cf89de9a828400f6b09bc3d8b4b8
mmalveres <54046185+mmalveres@users.noreply.github.com>
2024-08-16 21:33:26 -0300

    Create Dobro.java

---
+++ Dobro.java
@@ -0,0 +1,21 @@
+
+package br.com.controle;
+
+
+public class Dobro {
+   private double valor;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    public double calcularDobro(){
+     return this.valor*2;
+}
+    
+    
+    
+}
